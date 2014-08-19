f5092cb9fdf5d5adb02c8254d1d667cb890f2959
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-19 08:58:52 -0700

    rearranged param order in alm fxn

--- man-roxygen/alm_params.R
+++ man-roxygen/alm_params.R
@@ -2,13 +2,11 @@
 #' @param pmid PubMed object identifier (numeric)
 #' @param pmcid PubMed Central object identifier (numeric)
 #' @param mendeley Mendeley object identifier (character)
-#' @param url API endpoint, defaults to http://alm.plos.org/api/v3/articles (character)
 #' @param info One of totals, summary, or detail (default totals + sum_metrics data in a list). 
 #' 		Not specifying anything (the default) returns data.frame of totals across 
 #' 		data providers. (character)
 #' @param source Name of source (or list of sources) to get ALM information for (character)
 #' @param key your PLoS API key, either enter, or loads from .Rprofile (character)
-#' @param ... optional additional curl options (debugging tools mostly)
 #' @param total_details If FALSE (the default) the standard totals data.frame is
 #'    returned; if TRUE, the totals data is in a wide format with more details
 #'    about the paper, including publication date, title, etc. If you set this 
@@ -16,3 +14,5 @@
 #' @param sum_metrics Just like the output you get from setting info='totals', you can 
 #'    get summary metrics by day (sum_metrics='day'), month (sum_metrics='month'), 
 #'    or year (sum_metrics='year').
+#' @param url API endpoint, defaults to http://alm.plos.org/api/v3/articles (character)
+#' @param ... optional additional curl options (debugging tools mostly)
