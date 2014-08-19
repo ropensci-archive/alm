9dd3f5e67c5e0a38844a562d21d96bd1591f09a0
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-19 15:55:34 -0700

    fixed up alm_signposts fxn, working now :ant:

--- R/alm_ids.R
+++ R/alm_ids.R
@@ -55,8 +55,10 @@ alm_ids <- function(doi = NULL, pmid = NULL, pmcid = NULL, mendeley = NULL, info
 				}
 			}
 		if(info=="summary"){ 
-		  restmp <- if(length(id[[1]]) > 1) lapply(tt$data, getsummary) else getsummary(tt$data[[1]])
-      names(restmp) <- vapply(tt$data, function(x) x$doi, character(1))
+		  if(length(id[[1]]) > 1){
+		    restmp <- lapply(tt$data, getsummary) 
+        names(restmp) <- vapply(tt$data, function(x) x$doi, character(1))
+		  } else { restmp <- getsummary(tt$data[[1]]) }
       list(meta=metadf(tt), data=restmp)
     } else {		  
 			if(length(id[[1]]) > 1){
--- R/alm_signposts.R
+++ R/alm_signposts.R
@@ -8,26 +8,20 @@
 #'  \item citations: crossref (scopus at PLOS)
 #' }
 #' 
+#' @export
 #' @param doi Digital object identifier for an article in PLoS Journals (character)
 #' @param pmid PubMed object identifier (numeric)
 #' @param pmcid PubMed Central object identifier (numeric)
 #' @param mendeley Mendeley object identifier (character)
 #' @param url API endpoint, defaults to http://alm.plos.org/api/v3/articles (character)
-#' @param months Number of months since publication to request historical data for.
-#'    See details for a note. (numeric)
-#' @param days Number of days since publication to request historical data for. 
-#'    See details for a note. (numeric)
-#' @param year End of which year to request historical data for. 
-#'    See details for a note. (numeric)
 #' @param source Name of source (or list of sources) to get ALM information for 
 #'    (character)
 #' @param key your PLoS API key, either enter, or loads from .Rprofile (character)
 #' @param ... optional additional curl options (debugging tools mostly)
-#' @param curl If using in a loop, call getCurlHandle() first and pass
-#'    the returned value in here (avoids unnecessary footprint)
-#' @details This is just a wrapper around the function \code{\link{alm}}, forcing
+#' 
+#' @details This is just a wrapper around the function \code{\link{alm_ids}}, forcing
 #' info="summary", then coercing signposts data to a data.frame.
-#' @seealso \code{\link{alm}}, \code{\link{plot_signposts}}
+#' @seealso \code{\link{alm_ids}}, \code{\link{plot_signposts}}
 #' @return A data.frame of the signpost numbers for the searched object, and DOIs.
 #' @references See a tutorial/vignette for alm at 
 #' \url{http://ropensci.org/tutorials/alm_tutorial.html}
@@ -47,23 +41,21 @@
 #' # A single PubMed Central ID (pmcid)
 #' alm_signposts(pmcid=212692)
 #' 
-#' # A single Mendeley UUID (mendeley)
-#' alm_signposts(mendeley="35791700-6d00-11df-a2b2-0026b95e3eb7")
+#' # Curl debugging
+#' library('httr')
+#' alm_signposts(pmid=22590526, config=verbose())
 #' }
-#' @export
+
 alm_signposts <- function(doi = NULL, pmid = NULL, pmcid = NULL, mendeley = NULL, 
-                url = 'http://alm.plos.org/api/v3/articles',
-                months = NULL, days = NULL, year = NULL, 
-                source = NULL, key = NULL, curl = getCurlHandle())
+  source = NULL, key = NULL, url = 'http://alm.plos.org/api/v5/articles', ...)
 {	
-  temp <- alm(doi = doi, pmid = pmid, pmcid = pmcid, mendeley = mendeley, url = url, 
-      info = "summary", months = months, days = days, year = year, source = source, 
-      key = key, curl = curl)
-  if(class(try(temp[[2]], silent=TRUE)) == "try-error"){
-    data.frame(temp[[1]][c("views","shares","bookmarks","citations")])
+  temp <- alm_ids(doi = doi, pmid = pmid, pmcid = pmcid, mendeley = mendeley, url = url, 
+      info = "summary", source = source, key = key, ...)
+  if( !is.null(temp$data[['signposts']]) ){
+    temp$data[['signposts']]
   } else
   {
-    out <- lapply(temp, function(x) data.frame(x[c("doi","views","shares","bookmarks","citations")]))
-    do.call(rbind, out)
+    out <- lapply(temp$data, '[[', 'signposts')
+    do.call(rbind, unname(out))
   }
 }
