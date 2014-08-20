eb872456ddd6ec81d9bbe737fadd50ded0ea95ab
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-19 21:07:44 -0700

    fixed broken tests

--- tests/testthat/test-alm_pubmedcentid.R
+++ tests/testthat/test-alm_pubmedcentid.R
@@ -4,5 +4,5 @@ context("almpubmedcentid")
 key <- "rkfDr76z75benY3pytM1"
 
 test_that("almpubmedcentid returns the correct class", {
-	expect_that(almpubmedcentid('10.1371/journal.pbio.0000012', key=key), is_a("numeric"))
+	expect_that(alm_pubmedcentid('10.1371/journal.pbio.0000012', key=key), is_a("numeric"))
 })
---
+++ tests/testthat/test-alm_totals.R
@@ -0,0 +1,9 @@
+# tests for almtotals fxn in alm
+context("almtotals")
+
+key <- "rkfDr76z75benY3pytM1"
+
+test_that("almtotals returns the correct class", {
+	expect_that(alm_totals('10.1371/journal.pbio.0000012', key=key), is_a("data.frame"))
+	expect_that(alm_totals('10.1371/journal.pbio.1001357', key=key), is_a("data.frame"))
+})
