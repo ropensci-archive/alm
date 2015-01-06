332be5a3d5d9efc450f70a0c290582f4fcee1a6d
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-06 15:01:47 -0800

    rcharts removed from pkg, fix #42

--- R/plot_signposts.R
+++ R/plot_signposts.R
@@ -7,20 +7,10 @@
 #' @export
 #' 
 #' @param input A data.frame from a search from the \code{\link{alm_signposts}} function
-#' @param type Type of chart, one of bar (default), multiBarChart, or
-#'    multiBarHorizontalChart. multiBarChart or multiBarHorizontalChart
-#'    options use the library rCharts, specifically the NVD3 javascript library.
-#'    Choosing multiBarChart or multiBarHorizontalChart opens a visualization in
-#'    your default browser, while option bar makes a ggplot2 plot within your R session.
-#' @param width Only applies with type="interactive", otherwise ignored. Width of
-#'    the plotting area.
-#' @param height Only applies with type="interactive", otherwise ignored. Height of
-#'    the plotting area.
 #' @details Note that DOIs are the unit of replication of each study. When plotting,
 #' if the prefix is common among all DOIs, then just the end of the DOI, the numeric
 #' part is printed to make plots less ugly.
-#' @seealso \code{\link{alm_ids}}, \code{\link{plot_signposts}}
-#' @return A data.frame of the signpost numbers for the searched object.
+#' @seealso \code{\link{alm_signposts}}
 #' @references See a tutorial/vignette for alm at
 #' \url{http://ropensci.org/tutorials/alm_tutorial.html}
 #' @examples \dontrun{
@@ -33,23 +23,10 @@
 #'    '10.1371/journal.pone.0029797','10.1371/journal.pone.0039395')
 #' dat <- alm_signposts(doi=dois)
 #' plot_signposts(input=dat)
-#'
-#' # Or you can make an interactive chart
-#' plot_signposts(input=dat, type="multiBarChart")
-#'
-#' # Many DOIs
-#' library('rplos'); library('rCharts')
-#' dois <- searchplos(q='evolution', fl='id',
-#'    fq=list('-article_type:correction','doc_type:full'), limit = 10)
-#' dat <- alm_signposts(doi=dois$id)
-#' ## multiBarChart
-#' plot_signposts(input=dat, type="multiBarChart")
-#' ## multiBarHorizontalChart
-#' plot_signposts(input=dat, type="multiBarHorizontalChart")
 #' }
 
-plot_signposts <- function(input, type="bar", width=800, height=500)
-{
+plot_signposts <- function(input, width=800, height=500){
+  
   stripdois <- function(x){
     temp <- strsplit(as.character(x$doi), "\\.")
     if(all(sapply(temp, function(x) paste0(x[1:3],collapse="")) %in% sapply(temp, function(x) paste0(x[1:3],collapse=""))[[1]])){
@@ -57,8 +34,9 @@ plot_signposts <- function(input, type="bar", width=800, height=500)
       x <- x[,-1]
       x$doi <- doi_parts
       x
-    } else
-    { x }
+    } else { 
+      x 
+    }
   }
 
   if(!is.data.frame(input))
@@ -70,8 +48,7 @@ plot_signposts <- function(input, type="bar", width=800, height=500)
       theme_grey(base_size=16) +
       geom_bar(stat="identity") +
       labs(x="",y="")
-  } else
-  if(type=="bar"){
+  } else {
     input <- stripdois(input)
     df <- melt(input)
     ggplot(df, aes(doi, value)) +
@@ -79,14 +56,5 @@ plot_signposts <- function(input, type="bar", width=800, height=500)
       geom_bar(stat="identity") +
       facet_wrap(~variable, scales="free") +
       labs(x="",y="")
-  } else
-  {
-    input <- stripdois(input)
-    df <- melt(input)
-    n <- nPlot(value ~ doi, group = "variable", data = df, type = type)
-    n$set(width = width, height = height)
-    if(type=="multiBarChart")
-      n$chart(reduceXTicks = FALSE)
-    n
   }
 }
