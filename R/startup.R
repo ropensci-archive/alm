d8c1856c197482de708d9782dd416e53c43e22bf
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-23 22:36:40 -0700

    removed startup message

--- R/startup.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(...) {
-	packageStartupMessage("\n\n New to alm? Tutorial at http://ropensci.org/tutorials/alm_tutorial.html. Use suppressPackageStartupMessages() to suppress these startup messages in the future\n")
-} 
